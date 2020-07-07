5a8eff7f59a3002dbd2049350d6cb01570df6374
raspberian1234 <31656797+raspberian1234@users.noreply.github.com>
2020-07-07 13:20:03 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:37289097c7982cb3eb7e39d15632c56bdeb9f010f4e7c6dc48a139a9efe2bc86
+size 643
---
+++ testmodel.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f212517b8db205f2bf6b92694c4be91e5e31dce4b396bebc447fbb6c307384d8
+size 257
